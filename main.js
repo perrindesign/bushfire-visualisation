0e0e848a33dafd388397889fe96805c3c5896684
perrindesign <p.jones@columbia.edu>
2018-05-21 15:03:40 +1000

    Fit map to screen and resized

--- main.js
+++ main.js
@@ -1,87 +1,92 @@
+//Slider JS
+
+//Formatting the dates
 var formatDateIntoYear = d3.timeFormat("%Y");
-    var formatDate = d3.timeFormat("%b %Y");
+var formatDate = d3.timeFormat("%b %Y");
 
-    var startDate = new Date("2001-01-01"),
-      endDate = new Date("2015-12-01");
+var startDate = new Date("2001-01-01"),
+    endDate = new Date("2015-12-01");
 
-    var margin = { top: 0, right: 50, bottom: 0, left: 50 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+var margin = { top: 0, right: 50, bottom: 0, left: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 100 - margin.top - margin.bottom;
 
-    var svg = d3.select("#slider")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height);
+var svg = d3.select("#slider")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height);
 
-    var x = d3.scaleTime()
-      .domain([startDate, endDate])
-      .range([0, width])
-      .clamp(true);
+var x = d3.scaleTime()
+    .domain([startDate, endDate])
+    .range([0, width])
+    .clamp(true);
 
-    var slider = svg.append("g")
-      .attr("class", "slider")
-      .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
 
-    slider.append("line")
-      .attr("class", "track")
-      .attr("x1", x.range()[0])
-      .attr("x2", x.range()[1])
-      .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
-      .attr("class", "track-inset")
-      .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
-      .attr("class", "track-overlay")
-      .call(d3.drag()
+slider.append("line")
+    .attr("class", "track")
+    .attr("x1", x.range()[0])
+    .attr("x2", x.range()[1])
+    .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+    .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
         .on("start.interrupt", function () { slider.interrupt(); })
         .on("start drag", function () { hue(x.invert(d3.event.x)); }));
 
-    slider.insert("g", ".track-overlay")
-      .attr("class", "ticks")
-      .attr("transform", "translate(0," + 18 + ")")
-      .selectAll("text")
-      .data(x.ticks(10))
-      .enter()
-      .append("text")
-      .attr("x", x)
-      .attr("y", 10)
-      .attr("text-anchor", "middle")
-      .text(function (d) { return formatDateIntoYear(d); });
+slider.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+    .selectAll("text")
+    .data(x.ticks(10))
+    .enter()
+    .append("text")
+    .attr("x", x)
+    .attr("y", 10)
+    .attr("text-anchor", "middle")
+    .text(function (d) { return formatDateIntoYear(d); });
 
-    var label = slider.append("text")
-      .attr("class", "label")
-      .attr("text-anchor", "middle")
-      .text(formatDate(startDate))
-      .attr("transform", "translate(0," + (-25) + ")")
+var label = slider.append("text")
+    .attr("class", "label")
+    .attr("text-anchor", "middle")
+    .text(formatDate(startDate))
+    .attr("transform", "translate(0," + (-25) + ")")
 
-    var handle = slider.insert("circle", ".track-overlay")
-      .attr("class", "handle")
-      .attr("r", 9);
+var handle = slider.insert("circle", ".track-overlay")
+    .attr("class", "handle")
+    .attr("r", 9);
 
-    function hue(h) {
-      handle.attr("cx", x(h));
-      label
+function hue(h) {
+    handle.attr("cx", x(h));
+    label
         .attr("x", x(h))
         .text(formatDate(h));
-      //svg.style("background-color", d3.hsl(h/1000000000, 0.8, 0.8));
-    }
+    //svg.style("background-color", d3.hsl(h/1000000000, 0.8, 0.8));
+}
 
-    //Width and height
-    var w = 1000;
-    var h = 1000;
-    var areas = [];
+//Map JS
 
-    //Define path generator, using the geoMercator projection
+//Width and height
+var w = 1000;
+var h = 800;
+var areas = [];
 
-    var projection = d3.geoMercator()
-      .center([134, -25])
-      .scale(900)
-      .translate([w / 2, h / 2]);
+//Define path generator, using the geoMercator projection
 
-    var path = d3.geoPath()
-      .projection(projection);
+var projection = d3.geoMercator()
+    .center([134, -25])
+    .scale(800)
+    .translate([w / 2, h / 2]);
 
-    //Define quantize scale to sort data values into buckets of color
-    var color = d3.scaleQuantize()
-      .range(["rgb(0,0,0)",
+var path = d3.geoPath()
+    .projection(projection);
+
+//Define quantize scale to sort data values into buckets of color
+var color = d3.scaleQuantize()
+    .range(["rgb(0,0,0)",
         "rgb(50,50,50)",
         "rgb(75,75,75)",
         "rgb(105,105,105)",
@@ -94,72 +99,72 @@ var formatDateIntoYear = d3.timeFormat("%Y");
         "rgb(245,245,245)",
         "rgb(255,255,255)"]);
 
-    //Number formatting for population values
-    var formatAsThousands = d3.format(",");  //e.g. converts 123456 to "123,456"
+//Number formatting for population values
+var formatAsThousands = d3.format(",");  //e.g. converts 123456 to "123,456"
 
-    //Create SVG element
-    var svg = d3.select("body")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+//Create SVG element
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-    //Load in GeoJSON data
-    d3.json("SA2_2016_AUST.json").then(function (data) {
+//Load in GeoJSON data
+d3.json("australia.json").then(function (data) {
 
 
 
-      for (var i = 0; i < data.features.length; i++) {
+    for (var i = 0; i < data.features.length; i++) {
         //var n = data.features[i].properties.SA2_NAME16; 
         var a = data.features[i].properties.AREASQKM16;
         //names.push(n);
         areas.push(a);
-      }
+    }
 
-      //Set input domain for color scale
-      color.domain([
+    //Set input domain for color scale
+    color.domain([
         d3.min(areas),
         d3.max(areas)
-      ]);
+    ]);
 
-      //Bind data and create one path per GeoJSON feature
-      svg.selectAll("path")
+    //Bind data and create one path per GeoJSON feature
+    svg.selectAll("path")
         .data(data.features)
         .enter()
         .append("path")
         .attr("d", path)
         .style("fill", function (d) {
-          var value = d.properties.AREASQKM16;
-          //return color(value);
-          return "rgb(75,75,75)"
+            var value = d.properties.AREASQKM16;
+            //return color(value);
+            return "rgb(75,75,75)"
         })
         .style("stroke", "rgba(128,128,128,0.5)")
         .style("stroke-width", "1");
 
-      //Load in cities data
-      d3.csv("test.csv").then(function (data) {
+    //Load in cities data
+    d3.csv("test.csv").then(function (data) {
 
-        dataset = data;
+        //dataset = data;
 
         svg.selectAll("circle")
-          .data(data)
-          .enter()
-          .append("circle")
-          .attr("cx", function (d) {
-            return projection([d.longitude, d.latitude])[0];
-          })
-          .attr("cy", function (d) {
-            return projection([d.longitude, d.latitude])[1];
-          })
-          .attr("r", 5)
-          .style("fill", "yellow")
-          .style("stroke", "gray")
-          .style("stroke-width", 0.25)
-          .style("opacity", 0.75)
-          .append("title")			//Simple tooltip
-          .text(function (d) {
-            return d.place + ": Pop. " + formatAsThousands(d.population);
-          });
-
-      });
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+                return projection([d.longitude, d.latitude])[0];
+            })
+            .attr("cy", function (d) {
+                return projection([d.longitude, d.latitude])[1];
+            })
+            .attr("r", 5)
+            .style("fill", "yellow")
+            .style("stroke", "gray")
+            .style("stroke-width", 0.25)
+            .style("opacity", 0.75)
+            .append("title")			//Simple tooltip
+            .text(function (d) {
+                return d.place + ": Pop. " + formatAsThousands(d.population);
+            });
 
     });
+
+});
